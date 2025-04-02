39dde35abbc735c5c30e48074834e6c18ab10506
ABG <badzagueye@gmail.com>
2025-04-02 15:03:40 +0200

    Configuration pour déploiement Vercel

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,52 +3,70 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['react-refresh/babel'],
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction ? '[name].[contenthash].js' : 'bundle.js',
+      publicPath: '/',
+      clean: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+              plugins: [!isProduction && 'react-refresh/babel'].filter(Boolean),
+            },
           },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new ReactRefreshWebpackPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(process.env),
-    }),
-  ],
-  devServer: {
-    hot: true,
-    historyApiFallback: true,
-    port: 3003,
-  },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        favicon: './public/favicon.ico',
+        templateParameters: {
+          PUBLIC_URL: isProduction ? '.' : ''
+        }
+      }),
+      !isProduction && new ReactRefreshWebpackPlugin(),
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify({
+          ...process.env,
+          REACT_APP_API_URL: isProduction 
+            ? JSON.stringify('https://votre-api-de-production.com') 
+            : JSON.stringify(process.env.REACT_APP_API_URL || 'http://localhost:5000')
+        }),
+      }),
+    ].filter(Boolean),
+    devServer: {
+      hot: true,
+      historyApiFallback: true,
+      port: 3003,
+    },
+    performance: {
+      hints: isProduction ? 'warning' : false
+    },
+    devtool: isProduction ? 'source-map' : 'eval-source-map',
+  };
 }; 
